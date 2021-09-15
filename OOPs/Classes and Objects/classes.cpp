ca0464c84212b2c9bc0c80faba5b199884f47778
Kanav Arora <kanavarora1515@gmail.com>
2021-09-15 19:37:44 +0530

    Classes and Objects

---
+++ OOPs/Classes and Objects/classes.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+
+class Employee
+{
+    private:
+        int accountID;
+        float salary;
+    public:
+        int empID;
+        void setEmpId(int a);           // method declaration
+        void getDetails()
+        {
+            cout<<empID<<endl;
+            cout<<accountID<<endl;
+            cout<<salary<<endl;
+
+        }
+        void setDetails(int accID, float salary)
+        {
+            accountID = accID;
+            this->salary = salary;
+        }
+};
+
+void Employee :: setEmpId(int a){
+    Employee :: empID = a;
+}
+
+int main()
+{
+    int accID, id;
+    float salary;
+    cout<<"Bank Account No:"<<endl;
+    cin>>accID;
+    cout<<"Emp ID:"<<endl;
+    cin>>id;
+    cout<<"Salary:"<<endl;
+    cin>>salary;
+
+    Employee e1;
+    e1.setEmpId(id);
+    e1.setDetails(accID, salary);
+    e1.getDetails();
+    return 0;
+}
