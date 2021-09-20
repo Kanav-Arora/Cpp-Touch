b7d102afb041d3b496cec2ec29ef20626eca3f54
Kanav Arora <kanavarora1515@gmail.com>
2021-09-20 12:00:45 +0530

    Inheritance Files added

--- OOPs/Inheritance/inheritance.cpp
+++ OOPs/Inheritance/inheritance.cpp
@@ -5,9 +5,26 @@ using namespace std;
     Forms of Inheritance:
         - Single Inheritance
         - Multilevel Inheritance
+            Deriving a class from already derived class
+            Animal -> Mammal -> Human
         - Hierarchical Inheritance
+            One parent class has multiple sub classes
         - Hybrid Inheritance
+            Combination of multiple and multilevel inheritance
+                                A
+                                |
+                          ______ ______
+                         |             |
+                         B             C
+                         |             |
+                          ______ ______
+                                |
+                                D
         - Multiple Inheritamce
+            Derived class with more than one base class
+            Employee      -|
+                            ->  Programmer
+            Assistant     _| 
 */
 
 int main()
