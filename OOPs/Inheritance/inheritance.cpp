a3f0f2c02478933c8c8ef3e2547aacbbbe736e69
Kanav Arora <kanavarora1515@gmail.com>
2021-09-20 09:32:25 +0530

    Inheritance

---
+++ OOPs/Inheritance/inheritance.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+/*
+    Forms of Inheritance:
+        - Single Inheritance
+        - Multilevel Inheritance
+        - Hierarchical Inheritance
+        - Hybrid Inheritance
+        - Multiple Inheritamce
+*/
+
+int main()
+{
+    
+    return 0;
+}
