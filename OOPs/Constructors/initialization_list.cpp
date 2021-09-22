b391c02f4a4fa8d92bd3fff8b72b79f83f5f41ca
Kanav Arora <kanavarora1515@gmail.com>
2021-09-22 15:43:10 +0530

    Initialization list file added

--- OOPs/Constructors/initialization_list.cpp
+++ OOPs/Constructors/initialization_list.cpp
@@ -9,7 +9,7 @@ class A
         // A(int i, int j) : a(i), b(i+j)
         // A(int i, int j) : a(i), b(a +j)
         //* A(int i, int j) : b(j), a(i+b)  --> This method will raise error as initialization is done in order of declaration. So as a is declared first so it will be initialized first. But i+b will give garbage value as b is uninitialized.
-        
+        A(int i, int j): b(j), a(i)
         {
             cout<<"Value of a: "<<a<<endl
                 <<"Value of b: "<<b<<endl;
