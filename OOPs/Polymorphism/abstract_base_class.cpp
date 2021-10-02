f3e4faf1935cb15d08132cae3142ab24ad342a2a
Kanav Arora <kanavarora1515@gmail.com>
2021-10-02 22:21:15 +0530

    abstract_base_class.cpp file added

---
+++ OOPs/Polymorphism/abstract_base_class.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+/*
+    So in virtual function 
+    if derived class does not override the virtual function then base class function will be called
+
+    But with pure virtual function we are creating a condition that derived classes must override the virtual function
+
+    This means that the class with pure virtual function is made to be inherited.
+*/
+
+class BaseClass
+{
+    public:
+        int var_base;
+        virtual void display() = 0;   // do-nothing function --> pure virtual function
+};
+
+class DerivedClass : public BaseClass
+{
+    public:
+        int var_derived;
+        void display()
+        {
+            cout<<"Var_base value:"<<var_base<<endl;
+            cout<<"Var_derived value:"<<var_derived<<endl;
+        }
+};
+
+int main()
+{
+    /*
+        - Type of reference variable matters and not the object type assigned to it.
+        - BaseClass pointer can access data memebers of base class and not the derived class.
+        - Derived class pointer cannot point to base class object.
+    */
+
+    BaseClass *base_class_pointer;
+    // BaseClass obj_base;
+    DerivedClass obj_derived;
+    base_class_pointer = &obj_derived;
+    base_class_pointer->var_base = 30;
+    base_class_pointer->display();
+
+
+    DerivedClass *derived_class_pointer;
+    derived_class_pointer = &obj_derived;
+    derived_class_pointer->var_base = 60;
+    derived_class_pointer->var_derived = 40;
+    derived_class_pointer->display();
+    return 0;
+}
