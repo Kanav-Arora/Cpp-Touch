d3ecb0c7d368b6a6af535a921890cd79bf9f07bd
Kanav Arora <kanavarora1515@gmail.com>
2021-10-03 16:56:07 +0530

    creating.cpp file updated

--- Templates/creating.cpp
+++ Templates/creating.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
 using namespace std;
 
+template <class T>
+class Vector
+{
+    public:
+        T* arr;
+        int size;
+        Vector(int m)
+        {
+            size = m;
+            arr = new T[size];
+        }
+    T sumVec()
+    {
+        int sum=0;
+        for(int i=0; i<size; i++)
+        {
+            sum+= this->arr[i];
+        }
+        return sum;
+    }
+};
+
 int main()
 {
-    
+    Vector<int> myvec(4);
+    myvec.arr[0] = 4;
+    myvec.arr[1] = 5;
+    myvec.arr[2] = 6;
+    myvec.arr[3] = 7;
+    int sum = myvec.sumVec();
+    cout<<sum;
     return 0;
 }
