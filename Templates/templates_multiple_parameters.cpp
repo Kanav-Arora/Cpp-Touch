a11094f840c7faeb1fbfe203756ba63f4cb1b9ec
Kanav Arora <kanavarora1515@gmail.com>
2021-10-04 10:49:45 +0530

    templates_multiple_parameters.cpp file updated

--- Templates/templates_multiple_parameters.cpp
+++ Templates/templates_multiple_parameters.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
 using namespace std;
 
-
-
+template <class T1, class T2>
+class myclass
+{
+    public:
+        T1 data1;
+        T2 data2;
+        void display()
+        {
+            cout<<data1<<endl;
+            cout<<data2<<endl;
+        }
+};
 
 int main()
 {
-    
+    myclass<int,float> obj;
+    obj.data1 = 12;
+    obj.data2 = 1.2;
+    obj.display();
     return 0;
 }
