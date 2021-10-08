8b2ff3278676cdbba0aa28eb24720de6d4dcd465
Kanav Arora <kanavarora1515@gmail.com>
2021-10-08 21:47:59 +0530

    vector sorting files added

---
+++ Standard Template Library (STL)/Vectors/Sorting/desc_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> v1;
+    v1.push_back(2);
+    v1.push_back(1);
+    v1.push_back(9);
+    v1.push_back(4);
+    v1.push_back(3);
+
+    sort(v1.begin(), v1.end(), greater<int>());
+    for (auto x : v1)
+        cout << x << " ";
+
+    return 0;
+}
